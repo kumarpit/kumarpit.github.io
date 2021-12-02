4dd184cea1126c4529a339c4f3d7dd7a92b20ec8
kumarpit <kumar.arpit77@yahoo.com>
2021-12-01 19:16:39 -0800

    todo: animations + content

--- script/script.js
+++ script/script.js
@@ -1,3 +1,22 @@
+let projectButtons = document.getElementsByClassName("see-project");
+
+for (let i = 0; i < projectButtons.length; i++) {
+    projectButtons[i].addEventListener("click", function(){toggleDescription(projectButtons[i])})
+}
+
+function toggleDescription(button) {
+    if (button.classList.contains("expanded")) {
+        button.parentNode.parentNode.parentNode.children[1].style.display = "none"
+        button.innerHTML = '<i class="fas fa-chevron-down"></i>'
+    } else {
+        button.parentNode.parentNode.parentNode.children[1].style.display = "block"
+        button.innerHTML = '<i class="fas fa-chevron-up"></i>'
+    }
+
+    button.classList.toggle("expanded")
+}
+
+
 function toggleTheme() {
     let emoji = document.getElementById("theme-indicator")
 
@@ -5,11 +24,11 @@ function toggleTheme() {
         emoji.innerHTML = "&#127761"
         document.documentElement.style.setProperty('--clr-dark', '#f0f2f4');
         document.documentElement.style.setProperty('--clr-light', '#151515');
-        emoji.classList.remove("dark")
     } else {
         emoji.innerHTML = "&#9728"
         document.documentElement.style.setProperty('--clr-light', '#f0f2f4');
         document.documentElement.style.setProperty('--clr-dark', '#151515');
-        emoji.classList.add("dark")
     }
+
+    emoji.classList.toggle("dark")
 }
