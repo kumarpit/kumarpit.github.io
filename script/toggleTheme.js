88891b543bb813009bd6b23b73dc6e20611cf899
kumarpit <kumar.arpit77@yahoo.com>
2021-12-17 14:28:29 -0800

    colors

--- script/toggleTheme.js
+++ script/toggleTheme.js
@@ -4,13 +4,13 @@ icon.addEventListener("click", toggleTheme)
 function toggleTheme() {
     if (icon.classList.contains("dark")) {
         document.documentElement.style.setProperty('--clr-dark', '#1a1d23');
-        document.documentElement.style.setProperty('--clr-light', '#f1f1f1');
+        document.documentElement.style.setProperty('--clr-light', '#fff');
         document.documentElement.style.setProperty('--fc-dark', 'rgba(0, 0, 0, 0.5)');
         document.documentElement.style.setProperty('--fc-darker', 'rgba(0, 0, 0, 0.8)');
         document.documentElement.style.setProperty('--fc-darkest', '#000');
     } else {
         document.documentElement.style.setProperty('--clr-light', '#1a1d23');
-        document.documentElement.style.setProperty('--clr-dark', '#f1f1f1');
+        document.documentElement.style.setProperty('--clr-dark', '#fff');
         document.documentElement.style.setProperty('--fc-dark', 'rgba(255, 255, 255, 0.5)');
         document.documentElement.style.setProperty('--fc-darker', 'rgba(255, 255, 255, 0.8)');
         document.documentElement.style.setProperty('--fc-darkest', 'rgba(255, 255, 255, 0.9)');
