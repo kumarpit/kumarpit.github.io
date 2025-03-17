172032464b69340aede365a7c0973d114f280396
krarpit <kumar.arpit77@yahoo.com>
2025-03-16 21:55:53 -0700

    chore: refactors and fixed broken links

---
+++ src/pages/data/designTeams.ts
@@ -0,0 +1,7 @@
+export const designTeams: Array<{ title: string; desc: string; href: string }> = [
+  {
+    title: "Bionics",
+    desc: "Designed control software for a robotic prosthetic arm and developed signal processing pipelines to enable motor activation via EMG sensors",
+    href: "https://github.com/BEARUBC",
+  }
+];
